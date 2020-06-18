2842c9b502de0fea5d6bdde39c2a56e8eb94f2a7
liam <liamyabou@outlook.com>
2020-06-18 18:17:20 +0800

    detect content type

--- go/image_stream/stream.go
+++ go/image_stream/stream.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
     "io"
     "bytes"
+    urlParser "net/url"
+    "io/ioutil"
 )
 
 func main() {
@@ -20,6 +22,18 @@ func main() {
     buf := new(bytes.Buffer)
     buf.ReadFrom(response.Body)
     fmt.Printf("type: %v\n", http.DetectContentType(buf.Bytes()))
+    url = "https://images-na.ssl-images-amazon.com/images/I/01RmK%2BJ4pJL._AC_UL200_SR200,200_.gif"
+    decodedURL, err := urlParser.QueryUnescape(url)
+    if err != nil {
+        fmt.Printf("Error: %s\n", decodedURL)
+    }
+    fmt.Printf("decoded_url: %s\n", decodedURL)
+    response, e = http.Get(url)
+    if e != nil {
+        log.Fatal(e)
+    }
+    body, err := ioutil.ReadAll(response.Body)
+    fmt.Printf("decoded_url_type: %v\n", http.DetectContentType(body))
 }
 
 func getSize(stream io.Reader) int {
